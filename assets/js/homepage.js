a624917343465891102f14857e73ca00debc79bd
DiSantoz <dferreira0191@gmail.com>
2023-07-11 20:12:36 -0400

    add form and error for non user

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,20 @@
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
+
+let formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  let username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    nameInputEl.placeholder = "Please enter a Github Username";
+    userFormEl.classList.add("apply-shake");
+  }
+};
+
 let getUserRepos = function (users) {
   let apiUrl = "https://api.github.com/users/" + users + "/repos";
 
@@ -8,4 +25,4 @@ let getUserRepos = function (users) {
   });
 };
 
-getUserRepos("disantoz");
+userFormEl.addEventListener("submit", formSubmitHandler);
